835072af64043dd6ba36118a5e51da4106a58eaf
elemee3 <elemee3@github.com>
2022-05-07 15:21:50 -0700

    preserving api changes

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/* Using resource to allow easy access to other methods on this controller in the future */
+Route::resource('pets', 'App\Http\Controllers\PetController', ['only' => ['store']]);
